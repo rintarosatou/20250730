5ca5e32f3ebdc1177263d4e2ed9b5658d8477519
rinrin4470 <rinrin4470@gmail.com>
2025-07-30 11:06:12 +0900

    scene移行

---
+++ Assets/Script/ScenManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenManager : MonoBehaviour
+{
+
+    [SerializeField] private string nextSceneName = "NextScene";
+    [SerializeField] private string previousSceneName = "PreviousScene";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(previousSceneName);
+        }
+    }
+}
